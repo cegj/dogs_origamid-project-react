547fe4f0f963e8a9ae233c612fc3fbf0dff448d5
cegj <cegasparjr@gmail.com>
2022-08-14 19:46:48 -0300

    Class 817

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        <UserStorage>
+          <Header />
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login/*" element={<Login />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
-        <Footer />
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
